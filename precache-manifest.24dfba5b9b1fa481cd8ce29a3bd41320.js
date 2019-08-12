21bff039ebe4de69d29b74920470af9d2298e469
Peter Dulworth <pdulworth97@gmail.com>
2019-08-12 18:40:16 -0500

    Updates

---
+++ precache-manifest.24dfba5b9b1fa481cd8ce29a3bd41320.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0760a67f52b6220c3df8d49fcc1363a",
+    "url": "/website-2019-august/index.html"
+  },
+  {
+    "revision": "2303051dc295b5bcc575",
+    "url": "/website-2019-august/static/css/main.3a55ac05.chunk.css"
+  },
+  {
+    "revision": "fa776661158a274b3fae",
+    "url": "/website-2019-august/static/js/2.24002cfc.chunk.js"
+  },
+  {
+    "revision": "2303051dc295b5bcc575",
+    "url": "/website-2019-august/static/js/main.e3a67342.chunk.js"
+  },
+  {
+    "revision": "3cc47a036fe16096cd20",
+    "url": "/website-2019-august/static/js/runtime~main.3d618e5e.js"
+  },
+  {
+    "revision": "7d8251e7758d08870d9e40c1f4547e37",
+    "url": "/website-2019-august/static/media/blackhole.7d8251e7.png"
+  },
+  {
+    "revision": "40f650c4e9a590585c841e03634e9151",
+    "url": "/website-2019-august/static/media/prettypictures.40f650c4.png"
+  }
+]);
