a34e8e39e50c8c87af479d59384307e1cf7020fb
Peter Dulworth <pdulworth97@gmail.com>
2019-10-06 17:54:48 -0500

    added illustrator files

--- src/js/pages/ArtPage.js
+++ src/js/pages/ArtPage.js
@@ -1,10 +1,10 @@
-import React, { useContext } from 'react';
-import P5Wrapper from 'react-p5-wrapper';
-import sketch from '../sketches/sketch';
-import { ThemeContext } from '../contexts/themeContext';
+import React from 'react';
+// import P5Wrapper from 'react-p5-wrapper';
+// import sketch from '../sketches/sketch';
+// import { ThemeContext } from '../contexts/themeContext';
 
 const ArtPage = () => {
-  const theme = useContext(ThemeContext);
+  // const theme = useContext(ThemeContext);
   // const artPageThemeClass = theme.getThemeClass('Footer');
 
   return (
