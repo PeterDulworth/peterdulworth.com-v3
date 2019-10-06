09800667495dabbe3fd4775613de1e1baeb3931c
Peter Dulworth <pdulworth97@gmail.com>
2019-10-06 01:33:45 -0500

    Updates

---
+++ precache-manifest.fd4c88ea9da7d9731603bd76b1616a52.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "266442d331282725f9c3a2072f7470b9",
+    "url": "/peterdulworth.com-v3/index.html"
+  },
+  {
+    "revision": "4bf720673274d2b37502",
+    "url": "/peterdulworth.com-v3/static/css/main.ec501e8a.chunk.css"
+  },
+  {
+    "revision": "7d59de7b94c9f1320cd4",
+    "url": "/peterdulworth.com-v3/static/js/2.2b24b4bf.chunk.js"
+  },
+  {
+    "revision": "4bf720673274d2b37502",
+    "url": "/peterdulworth.com-v3/static/js/main.fcbbc91a.chunk.js"
+  },
+  {
+    "revision": "c08df8b0d589235afd12",
+    "url": "/peterdulworth.com-v3/static/js/runtime~main.8156b62b.js"
+  },
+  {
+    "revision": "7d8251e7758d08870d9e40c1f4547e37",
+    "url": "/peterdulworth.com-v3/static/media/blackhole.7d8251e7.png"
+  },
+  {
+    "revision": "ccebfbabe22f068d2360647441aff906",
+    "url": "/peterdulworth.com-v3/static/media/companyinsights.ccebfbab.png"
+  },
+  {
+    "revision": "a62c75d35789c53a31719a520d46c834",
+    "url": "/peterdulworth.com-v3/static/media/dogstickers.a62c75d3.png"
+  },
+  {
+    "revision": "8c063cac75189026d71c60927cd1bdab",
+    "url": "/peterdulworth.com-v3/static/media/easel.8c063cac.svg"
+  },
+  {
+    "revision": "1f12c539e89251971b117a308975650b",
+    "url": "/peterdulworth.com-v3/static/media/franz.1f12c539.png"
+  },
+  {
+    "revision": "8ed9966738710fe5376e1840b4b980cf",
+    "url": "/peterdulworth.com-v3/static/media/indeed.8ed99667.svg"
+  },
+  {
+    "revision": "99c5e4bb1538b11d883f63ed2717086d",
+    "url": "/peterdulworth.com-v3/static/media/josie_ball.99c5e4bb.jpg"
+  },
+  {
+    "revision": "6bd5a0e5325bceae8873fa132c8ab054",
+    "url": "/peterdulworth.com-v3/static/media/josie_basket.6bd5a0e5.jpg"
+  },
+  {
+    "revision": "45d7d73e789a12f5f7ce8de50f87795e",
+    "url": "/peterdulworth.com-v3/static/media/josie_box.45d7d73e.jpg"
+  },
+  {
+    "revision": "96886bcdc046c2bcaf9723a86658a255",
+    "url": "/peterdulworth.com-v3/static/media/moodsic.96886bcd.png"
+  },
+  {
+    "revision": "f3bd89d7c0ab78b65178e3d9a7dfcc3c",
+    "url": "/peterdulworth.com-v3/static/media/other.f3bd89d7.png"
+  },
+  {
+    "revision": "0c070d5c04fd0421182be37cd6fd2834",
+    "url": "/peterdulworth.com-v3/static/media/prettypictures.0c070d5c.png"
+  },
+  {
+    "revision": "657b0b70a7e7d02191e1166f358e72ff",
+    "url": "/peterdulworth.com-v3/static/media/rice.657b0b70.svg"
+  },
+  {
+    "revision": "eb27462f23e3cab7804958bb17be94ce",
+    "url": "/peterdulworth.com-v3/static/media/sketches.eb27462f.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/peterdulworth.com-v3/precache-manifest.23d034e9d049242facf6be9099d1c5cd.js"
+  "/peterdulworth.com-v3/precache-manifest.fd4c88ea9da7d9731603bd76b1616a52.js"
 );
 
 self.addEventListener('message', (event) => {
