553c6240197cdf8a48fe2ecd43f1a89198a27fd1
Peter Dulworth <pdulworth97@gmail.com>
2019-10-06 21:48:36 -0500

    cleaned up rectangle sketch

--- src/js/sketches/rectanglesSketch.js
+++ src/js/sketches/rectanglesSketch.js
@@ -1,4 +1,17 @@
 const rectanglesSketch = (p) => {
+  const MAX_WIDTH = 900;
+  const DARK_COLOR = 22;
+  const LIGHT_COLOR = 255;
+  const X_STEP = 10;
+  const Y_STEP = 10;
+  // alternative constants:
+  // const X_STEP = 10;
+  // const Y_STEP = 1;
+  // const X_STEP = 4;
+  // const Y_STEP = 4;
+  // const X_STEP = 1;
+  // const Y_STEP = 1;
+
   let isDarkMode = false;
   let isSetup = false;
   let y = 0;
@@ -7,7 +20,6 @@ const rectanglesSketch = (p) => {
     const parentDiv = document.getElementById('rectanglesSketch');
     const parentWidth = parentDiv.offsetWidth;
     const parentHeight = parentDiv.offsetHeight;
-    const MAX_WIDTH = 900;
 
     p.createCanvas(parentWidth < MAX_WIDTH ? parentWidth : MAX_WIDTH, parentHeight);
     p.strokeWeight(0);
@@ -18,7 +30,7 @@ const rectanglesSketch = (p) => {
 
   p.reset = () => {
     p.clear();
-    p.background(isDarkMode ? 22 : 255); // try 22
+    p.background(isDarkMode ? DARK_COLOR : LIGHT_COLOR);
     y = 0;
     p.loop();
   };
@@ -30,28 +42,7 @@ const rectanglesSketch = (p) => {
     }
   };
 
-  /**
-   * Interesting constant values:
-   */
-
-  // #1
-  const X_STEP = 10;
-  const Y_STEP = 10;
-
-  // #2
-  // const X_STEP = 10;
-  // const Y_STEP = 1;
-
-  // #3
-  // const X_STEP = 4;
-  // const Y_STEP = 4;
-
-  // #4
-  // const X_STEP = 1;
-  // const Y_STEP = 1;
-
   p.draw = () => {
-    // draw ring of circles
     for (let x = 0; x < p.width; x += X_STEP) {
       p.fill(p.random(150, 220));
       p.rect(x, y, p.random(4), p.random(4));
